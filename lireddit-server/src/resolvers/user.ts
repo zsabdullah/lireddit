afb5cf03fb525316315e23b691600c85fcb9c93e
Zubair Abdullah <31964315+zsabdullah@users.noreply.github.com>
2021-08-23 02:27:27 -0500

    added basic registration

---
+++ lireddit-server/src/resolvers/user.ts
@@ -0,0 +1,42 @@
+import { User } from "../entities/User";
+import { MyContext } from "src/types";
+import {
+  Resolver,
+  Mutation,
+  Arg,
+  Field,
+  Ctx,
+  InputType,
+  Query,
+} from "type-graphql";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Query(() => [User])
+  users(@Ctx() { em }: MyContext): Promise<User[]> {
+    return em.find(User, {});
+  }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) {
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
+}
