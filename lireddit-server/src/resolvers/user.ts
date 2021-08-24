d4a9492f3ee118525a965cf6e68db1b54a4f1867
Zubair Abdullah <31964315+zsabdullah@users.noreply.github.com>
2021-08-24 00:11:43 -0500

    added registration error checks & started cookies

--- lireddit-server/src/resolvers/user.ts
+++ lireddit-server/src/resolvers/user.ts
@@ -8,6 +8,7 @@ import {
   Ctx,
   InputType,
   Query,
+  ObjectType,
 } from "type-graphql";
 import argon2 from "argon2";
 
@@ -19,6 +20,30 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+// to fix userId not working
+declare module "express-session" {
+  interface Session {
+    userId: number;
+  }
+}
+
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
@@ -26,17 +51,90 @@ export class UserResolver {
     return em.find(User, {});
   }
 
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
+    if (options.password.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "length must be greater than 2",
+          },
+        ],
+      };
+    }
+
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
     });
-    await em.persistAndFlush(user);
-    return user;
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      // duplicate username err
+      if (err.code === "23505") {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "username has already been taken.",
+            },
+          ],
+        };
+      }
+      console.log("message:", err.message);
+    }
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {
+      username: options.username.toLowerCase(),
+    });
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "that username does not exist",
+          },
+        ],
+      };
+    }
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "incorrect password",
+          },
+        ],
+      };
+    }
+
+    req.session.userId = user.id;
+
+    return {
+      user,
+    };
   }
 }
--- lireddit-server/src/types.ts
+++ lireddit-server/src/types.ts
@@ -1,5 +1,10 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core";
+import { Request, Response } from "express";
+import { Session, SessionData } from "express-session";
 
 export type MyContext = {
-  em: EntityManager<IDatabaseDriver<Connection>>;
+  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
+  req: Request & { session: Session & Partial<SessionData> };
+  // req: Request & { session: Session & Partial<SessionData> };
+  res: Response;
 };
